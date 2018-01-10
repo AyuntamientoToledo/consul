474d77e3f696e1f41b4ec9eec6d94bf6e3382e82
alberto <alberto.astudillo@wearestrings.com>
2018-01-10 18:10:29 +0100

    feature allow images

---
+++ db/migrate/20180110170929_feature_allow_images.rb
@@ -0,0 +1,9 @@
+class FeatureAllowImages < ActiveRecord::Migration
+  def up
+    Setting['feature.allow_images'] = true
+  end
+
+  def down
+    Setting.['feature.allow_images'].destroy
+  end
+end
