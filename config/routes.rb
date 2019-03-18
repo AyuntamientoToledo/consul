5f5cadb55979e6d7b61aeebc042bce7736d2950a
Pedro Leon <pedro@MacBook-Pro-de-Pedro.local>
2019-03-18 15:42:36 +0100

    Removed stats page

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
   get '/welcome', to: 'welcome#welcome'
   get '/consul.json', to: "installation#details"
 
-  resources :stats, only: [:index]
+ # resources :stats, only: [:index]
   resources :images, only: [:destroy]
   resources :documents, only: [:destroy]
   resources :follows, only: [:create, :destroy]
